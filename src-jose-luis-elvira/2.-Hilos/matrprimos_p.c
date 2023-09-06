9984b2985343327f5b1ad50fd43c1429cbfe652a
Leonardo Sandoval <leo.san.gon@gmail.com>
2023-09-06 12:53:17 -0600

    populate matrix with a simple counter
    Besides making the matrix deterministic, include a function which
    contains the double-loop to find primes. This would prepare the land
    for pthreads
    
    Signed-off-by: Leonardo Sandoval <leo.san.gon@gmail.com>

--- src-jose-luis-elvira/2.-Hilos/matrprimos_p.c
+++ src-jose-luis-elvira/2.-Hilos/matrprimos_p.c
@@ -12,9 +12,32 @@
 
 int mat[SIZE][SIZE];
 
+#define LOOPS 4
+
 void initmat(int mat[][SIZE]);
 void printnonzeroes(int mat[SIZE][SIZE]);
 int isprime(int n);
+void printmat(int mat[][SIZE]);
+void *tfunc(void *args);
+
+void *tfunc(void *args)
+{
+  /* compute index ranges */
+  int tnum = *((int *) args);
+  int i_start = tnum * SIZE/LOOPS;
+  int i_end = (1+tnum) * SIZE/LOOPS;  
+
+  //printf("tnum %d i_start %d i_end %d\n",tnum, i_start, i_end);
+
+  int i,j;
+  for(i=i_start;i<i_end;i++)
+    for(j=0;j<SIZE;j++)
+      if(!isprime(mat[i][j])) {
+	//printf("(%d,%d)=%d is not prime\n", i, j, mat[i][j]);
+	mat[i][j]=0;
+      }
+}	
+
 
 int main()
 {
@@ -23,21 +46,21 @@ int main()
 	long long elapsed_time;
 	long lElapsedTime;
 	struct timeval ts;
-	int i,j;
+	int i;
 
 	// Inicializa la matriz con números al azar
 	initmat(mat);
 	
+	// Imprimit  la matriz
+	//printmat(mat);
+
 	gettimeofday(&ts, NULL);
 	start_ts = ts.tv_sec; // Tiempo inicial
 
 	// Eliminar de la matriz todos los números que no son primos
 	// Esta es la parte que hay que paralelizar
-	
-	for(i=0;i<SIZE;i++)
-		for(j=0;j<SIZE;j++)
-			if(!isprime(mat[i][j]))
-			   mat[i][j]=0;
+	for(i=0;i<LOOPS;i++)
+	  tfunc(&i);
 	
 	// Hasta aquí termina lo que se tiene que hacer en paralelo
 	gettimeofday(&ts, NULL);
@@ -53,12 +76,22 @@ int main()
 void initmat(int mat[][SIZE])
 {
 	int i,j;
-	
+	int count=0;	
 	srand(getpid());
 	
 	for(i=0;i<SIZE;i++)
-		for(j=0;j<SIZE;j++)
-			mat[i][j]=INICIAL+rand()%(FINAL-INICIAL);
+	  for(j=0;j<SIZE;j++)
+	    mat[i][j]=count++;
+	    //mat[i][j]=INICIAL+rand()%(FINAL-INICIAL);
+}
+
+void printmat(int mat[][SIZE])
+{
+  int i,j;
+	
+  for(i=0;i<SIZE;i++)
+    for(j=0;j<SIZE;j++)
+      printf("%d ", mat[i][j]);
 }
 
 void printnonzeroes(int mat[SIZE][SIZE])
